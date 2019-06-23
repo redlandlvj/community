2c7813aee640405ffd895982947e7ea6a9bae84c
redlandman <364503188@qq.com>
2019-06-23 20:55:23 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by 吕俊 on 2019/6/23.
+ */
+@Controller
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
+
